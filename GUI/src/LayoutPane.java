b65407bbda5df32011181a404481f34ef7211a71
RyotoTomioka <tommy.ryoto93@gmail.com>
2016-06-01 22:13:42 +0900

    lightの実装開始

--- GUI/src/LayoutPane.java
+++ GUI/src/LayoutPane.java
@@ -19,21 +19,32 @@ public class LayoutPane extends JLayeredPane{
         setLight_layout();
         sensor_layout = new JPanel();
 
-        this.add(light_layout, JLayeredPane.DEFAULT_LAYER);
         this.add(room_layout, JLayeredPane.DEFAULT_LAYER);
+        this.add(light_layout, JLayeredPane.DEFAULT_LAYER);
+
 
     }
 
     public void setRoom_layout() {
         room_layout = new JSVGCanvas();
-        room_layout.setURI("GUI/KC111_Ceiling.svg");
-        room_layout.setBounds(0,0, getWidth(), getHeight());
+        room_layout.setURI("GUI/KC111_Ceiling_op.svg");
+        room_layout.setBounds(0,10, getWidth(), getHeight()-40);
+        room_layout.setOpaque(false);
     }
 
     public void setLight_layout() {
         light_layout = new JPanel();
         light_layout.setOpaque(false);
         light_layout.setBounds(0, 0, getWidth(), getHeight());
+        light_layout.setLayout(null);
+
+        JSVGCanvas lightCanvas[] = new JSVGCanvas[12];
+        for(int i = 0; i<12; i++) {
+            lightCanvas[i] = new JSVGCanvas();
+            lightCanvas[i].setURI("GUI/light.svg");
+            lightCanvas[i].setBounds(149+(i/3)*151,14+(i%3)*151, 48, 48);
+            light_layout.add(lightCanvas[i]);
+        }
     }
 
 }
